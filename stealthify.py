9180b6aa47eb2a7a4611166dc02a367f627b9340
Shrivu Shankar <shrivu1122@gmail.com>
2018-12-12 16:09:36 -0600

    Added CLI

--- stealthify.py
+++ stealthify.py
@@ -1,7 +1,9 @@
+# Use lib as if they were installed
 import sys
 sys.path.insert(0, "./InPainting")
 sys.path.insert(0, "./MaskRCNN")
 
+# Utils
 import skimage.io
 import skimage.transform
 import cv2
@@ -12,20 +14,49 @@ import find_mask
 import inpaint
 
 
-def main(fn, thing):
-
-    image = skimage.io.imread(fn)
-    # image = skimage.transform.resize(image, (320, 320)) * 255
-
-    class_mask = find_mask.find(image, thing)
-    
+def inflate_mask(mask):
+    """Increase the size of the masked region"""
     kernel = np.ones((16, 16), np.uint8)
-    class_mask = cv2.dilate(class_mask, kernel, 1)
-    
-    result = inpaint.remove_masked(image, class_mask)
+    return cv2.dilate(mask, kernel, 1)
 
-    PIL.Image.fromarray(result.astype(np.uint8)).show()
+
+def get_image(array):
+    """Convert array(h, w, 3) to image object"""
+    return PIL.Image.fromarray(array.astype(np.uint8))
+
+
+def main(fn, thing, display=True, save_fn=None):
+    """Remove `thing` from image"""
+    image_ary = skimage.io.imread(fn)
+
+    class_mask = find_mask.find(image_ary, thing)
+    class_mask = inflate_mask(class_mask)
+    
+    result = inpaint.remove_masked(image_ary, class_mask)
+
+    image = get_image(result)
+
+    if display:
+        image.show()
+
+    if save_fn:
+        image.save(save_fn)
 
 
 if __name__ == "__main__":
-    main('test.jpg', 'giraffe')
+
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument("-f", "--file", dest="filename",
+                        help="The image file to use", metavar="filename", required=True)
+    parser.add_argument("-c", "--class", dest="class_name", choices=find_mask.class_names, type=str,
+                        help="The type of object to hide", metavar="class_name", nargs=1, required=True)
+    parser.add_argument("-d", "--display", dest="display", action='store_true',
+                        help="Display the resulting image")
+    parser.add_argument("-s", "--save", dest="save_file", default='output.jpg', nargs='?',
+                        help="Where to save the resulting image", metavar="save_file")
+
+    args = parser.parse_args()
+
+    main(args.filename, args.class_name[0], display=args.display, save_fn=args.save_file)
